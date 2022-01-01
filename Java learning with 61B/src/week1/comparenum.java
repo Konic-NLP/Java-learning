e38ad18d2c2898d4db807038f233a1d18181ca0e
Sijia(Konic) Ge <94318777+Konic-NLP@users.noreply.github.com>
2022-01-01 01:29:35 -0700

    Add files via upload

---
+++ Java learning with 61B/src/week1/comparenum.java
@@ -0,0 +1,31 @@
+package week1;
+
+public class comparenum {
+	
+	
+	public static int Larger(int x, int y) {
+		if (x>y) {
+			return x;
+		
+			
+			
+		}
+		return y;
+	}
+public static void main(String[] args) {
+	
+	System.out.println(Larger(5,-10));
+	
+	
+	
+	
+	
+}
+}
+
+/*1. in Java, using public static to define a function
+ * 2. all orarameters of a function have a type
+ * and the function itself must have a return tyoe
+ * 3. the function inside a class called 'method', so all functions in Java are methods
+ *  */
+ 
